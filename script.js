3e5aa9f76fc54187457113d1d947f279a06251bd
MFRRayhan <md.fazlerabbirayhan786@gmail.com>
2025-04-15 00:05:34 +0600

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
+    
     const playBtn = document.getElementById('play-btn');
     const closeBtn = document.getElementById('close-btn');
     const videoPopup = document.getElementById('video-popup');
     const popupVideo = document.getElementById('popup-video');
 
-    playBtn.addEventListener('click',  () => {
+    playBtn.addEventListener('click', () => {
         videoPopup.style.display = 'flex';
 
         setTimeout(() => {
@@ -17,5 +18,16 @@ document.addEventListener("DOMContentLoaded", function () {
         videoPopup.style.display = 'none';
         popupVideo.pause();
         popupVideo.currentTime = 0;
-    })
-})
+    });
+
+    window.addEventListener('scroll', function () {
+        const header = document.querySelector('header');
+        const stickyPoint = 120;
+
+        if (window.scrollY >= stickyPoint) {
+            header.classList.add('sticky-header');
+        } else {
+            header.classList.remove('sticky-header');
+        }
+    });
+});
